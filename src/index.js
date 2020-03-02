d884219fcbb7cb0789cdc642d3be4451e4d1b101
Osmar Carlos <osmarcarlosrm@gmail.com>
2020-03-02 11:09:23 -0600

    Init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDom.render(<p>Hi</p> ,document.getElementById('app'));
